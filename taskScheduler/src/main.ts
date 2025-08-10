99f8876ad85369d715d8f25be84357d4b2405ae8
AtsuyaShimizu <atsuya252172@gmail.com>
2025-08-10 23:46:41 +0900

    fix: router provider

--- taskScheduler/src/main.ts
+++ taskScheduler/src/main.ts
@@ -3,12 +3,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { SchedulePage } from './app/page/schedule/schedule.page';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 import { SCHEDULE_SERVICE } from './app/di/tokens';
 import { MockScheduleService } from './app/domain/service/impl/mock-schedule.service';
 
 bootstrapApplication(SchedulePage, {
   providers: [
-    routes,
+    provideRouter(routes),
     provideHttpClient(),
     { provide: SCHEDULE_SERVICE, useExisting: MockScheduleService },
   ],
